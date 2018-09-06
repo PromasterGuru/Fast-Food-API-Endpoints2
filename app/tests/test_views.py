228721de3b6e8280169a20049e607a5a0f35cd4b
root <root@localhost.localdomain>
2018-09-06 14:44:08 +0300

    [Chore: #160312568] Add admin user can fetch as specific order

--- app/tests/test_views.py
+++ app/tests/test_views.py
@@ -27,7 +27,7 @@ class RouteTestCases(unittest.TestCase):
         resp = self.client().post('/api/v1/orders', data=self.order)
         self.assertEqual(resp.status_code, 201)
         self.assertIn('Tater tots', str(resp.data))
-    
+
     def test_user_can_get_all_orders(self):
         '''Test API can return all the orders (GET request)'''
         resp = self.client().post('/api/v1/orders', data=self.order)
@@ -35,3 +35,13 @@ class RouteTestCases(unittest.TestCase):
         resp = self.client().get('/api/v1/orders')
         self.assertEqual(resp.status_code, 200)
         self.assertIn('Tater tots', str(resp.data))
+
+    def test_user_can_fetch_a_specific_order(self):
+        '''Test API can return a specific order using its id (GET request)'''
+        resp = self.client().post('/api/v1/orders', data=self.order)
+        self.assertEqual(resp.status_code, 201)
+        result_in_json = json.loads(resp.data.decode('utf-8').replace("'", "\""))
+        result = self.client().get(
+            '/api/v1/orders/{}'.format(result_in_json['id']))
+        self.assertEqual(resp.status_code, 200)
+        self.assertIn('Tater tots', str(result.data))
